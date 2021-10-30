1453a8202a79b5ff3508e8ba7d52693515dd4322
kmakai <makaikwag@gmail.com>
2021-10-30 17:45:46 -0400

    got the etch-a-sketch to finally work

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const container = document.createElement('div')
+const container = document.createElement('div');
+
 
 container.className = 'container';
 document.body.appendChild(container);
@@ -9,11 +10,11 @@ for (i = 0; i < 16; i++) {
     for (j = 0; j < 16; j++) {
         let cell = document.createElement('div');
         row.appendChild(cell).className = "cell";
+    cell.addEventListener("mouseover",function(e) {
+    let spot = e.target;
+    spot.style.backgroundColor = "red";
+})
+    
     }
+
 }
-
-
-
-container.appendChild(row);
-const gridItem = document.querySelectorAll("cell");
-
