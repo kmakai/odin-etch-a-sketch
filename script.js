d86ddb188b78b8f6878aac83c1aa70c5741b089a
kmakai <makaikwag@gmail.com>
2021-10-30 20:14:34 -0400

    aa

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const container = document.createElement('div');
-
-
+let gridRows;
+let gridColumns;
+let row;
+let cell;
+let spot;
+let k;
 container.className = 'container';
 document.body.appendChild(container);
-for (i = 0; i < 16; i++) {
-    let row = document.createElement('div');
-    container.appendChild(row).className = "row";
+function creatGrid(gridRows, gridColumns) {
+    for (i = 0; i < gridRows; i++) {
+        row = document.createElement('div');
+        container.appendChild(row).className = "row";
 
-    for (j = 0; j < 16; j++) {
-        let cell = document.createElement('div');
-        row.appendChild(cell).className = "cell";
-    cell.addEventListener("mouseover",function(e) {
-    let spot = e.target;
-    spot.style.backgroundColor = "red";
-})
-    
+        for (j = 0; j < gridColumns; j++) {
+            cell = document.createElement('div');
+            row.appendChild(cell).className = "cell";
+            cell.addEventListener("mouseover", function (e) {
+                spot = e.target;
+                spot.style.backgroundColor = "red";
+            })
+
+        }
+
     }
-
 }
+
+
+
+
+
+gridRows = prompt("how many rows do you want? ");
+gridColumns = prompt("how many columns do you want?");
+creatGrid(gridRows,gridColumns);
